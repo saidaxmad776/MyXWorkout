3406502519c171f6d930d3709eb7bc50857c3e6f
ASaidaxmad <saidaxmad7760746@gmail.com>
2022-08-06 18:23:33 +0500

    Set up Views,

---
+++ MyXWorkout/Controller/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  MyXWorkout
+//
+//  Created by Test on 06/08/22.
+//
+
+import UIKit
+
+class MainVC: ViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
