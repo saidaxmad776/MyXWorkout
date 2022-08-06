c986699677732c69be931654f36fd0cf1437f921
ASaidaxmad <saidaxmad7760746@gmail.com>
2022-08-06 09:51:50 +0500

    Initial Commit

---
+++ MyXWorkout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyXWorkout
+//
+//  Created by Test on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
